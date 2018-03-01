6e7ba37e67e64b4c3b43fdb14ed84f98849f1d57
timmoore226 <timmoore226@gmail.com>
2018-02-28 22:09:51 -0500

    Work on maps
    Added map detail view.  Only pulls up default location for now.

---
+++ Food Pin/View/RestaurantDetailMapCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantDetailMapCell.swift
+//  Food Pin
+//
+//  Created by Timothy Moore on 2/28/18.
+//  Copyright © 2018 Tim Moore. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RestaurantDetailMapCell: UITableViewCell {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Food Pin/View/RestaurantDetailSeparatorCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailSeparatorCell.swift
+//  Food Pin
+//
+//  Created by Timothy Moore on 2/28/18.
+//  Copyright © 2018 Tim Moore. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailSeparatorCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
