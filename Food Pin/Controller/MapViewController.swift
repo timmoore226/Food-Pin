5f3014dc6b72303f701937282d4b66e4295f7375
timmoore226 <timmoore226@gmail.com>
2018-03-02 12:56:57 -0500

    Chapter 16 complete
    Finished custom annotation for map view.

--- Food Pin/Controller/MapViewController.swift
+++ Food Pin/Controller/MapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet var mapView: MKMapView!
     
@@ -17,6 +17,12 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
+        
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
         
         // Convert address to coordinate and annotate it on map
         let geoCoder = CLGeocoder()
@@ -45,7 +51,6 @@ class MapViewController: UIViewController {
             }
             
         })
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,15 +58,24 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyMarker"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        annotationView?.glyphText = "😋"
+        annotationView?.markerTintColor = UIColor.orange
+        
+        return annotationView
     }
-    */
 
 }
